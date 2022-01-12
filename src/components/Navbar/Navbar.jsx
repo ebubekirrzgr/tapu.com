26fa5dd3db84ff87ac2e265c41b509b48f0230e7
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2022-01-12 22:43:02 +0300

    navbar bugs fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import './navbar.scss';
 
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import listIcon from '../../assets/png/list.png';
 import accountIcon from '../../assets/png/profile.png';
 
@@ -9,15 +11,22 @@ function Navbar() {
   return (
     <div className="navbarWrapper">
       <div className="navbarList">
-        <img src={listIcon} alt="listIcon" />
-        <p className="navbarText">List</p>
+        <Link to="/Home" className="listButton">
+          <img src={listIcon} alt="listIcon" />
+          <p className="navbarText">List</p>
+        </Link>
       </div>
       <div className="navbarAccount">
-        <img src={accountIcon} alt="accountIcon" />
-        <p className="navbarText">Account</p>
+        <Link to="/Account" className="listButton">
+          <img src={accountIcon} alt="accountIcon" />
+          <p className="navbarText">Account</p>
+        </Link>
       </div>
     </div>
   );
 }
 
 export default Navbar;
+
+// <img src={listIcon} alt="listIcon" />
+//         <p className="navbarText">List</p>
